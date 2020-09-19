6187a4aebfc2759a6853345e6fc634ff791f4534
sarahrasheed19 <sarahrasheed@mail.adelphi.edu>
2020-09-19 19:53:29 -0400

    moved flask-security objects around

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore,
 from eBookClub import settings
-from models import user_datastore, security 
+from models import User, Role
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.MYSQL_URI
 app.config['SECRET_KEY'] = settings.SECRETKEY
 db = SQLAlchemy(app)
+
+#setup flask-security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
