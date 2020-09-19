ca67733eabbdb21688b0393bcd3eb728af412234
kaitlyntorres <71412255+kaitlyntorres@users.noreply.github.com>
2020-09-19 12:35:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
-
+from flask import Flask, render_template, redirect, url_for, request
+# Route for handling the login page logic
 app = Flask(__name__)
-
 @app.route('/')
-def index():
-    return "Hello, World!"
+def home():
+    return "Welcome to Home Page!"
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid username or password'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 if __name__ == "__main__":
     app.run(debug=True)
