6e09590083d159bd8b19bdc17bf9c4c248d4e135
kaitlyntorres <71412255+kaitlyntorres@users.noreply.github.com>
2020-09-19 22:21:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = settings.MYSQL_URI
 app.config['SECRET_KEY'] = settings.SECRETKEY
 db = SQLAlchemy(app)
 
-user_datastore=SQLAlchemyUserDatastore(db, 'User', 'Role')
+user_datastore=SQLAlchemyUserDatastore(db, 'user_id', 'role_id')
 security=Security(app,user_datastore)
 
 @app.route('/register', methods=['POST','GET'])
