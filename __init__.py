2f500f0b704eb5c6b0ccc0709828fa900d24e7f9
root <sarahrasheed@mail.adelphi.edu>
2020-09-20 02:36:53 +0000

    needs fixes

--- __init__.py
+++ __init__.py
@@ -1,12 +1,2 @@
-from . import settings
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = settings.MYSQL_URI
-app.config['SECRET_KEY'] = settings.SECRETKEY
-
-db = SQLAlchemy(app) 
+from  . import settings
+import pymysql
